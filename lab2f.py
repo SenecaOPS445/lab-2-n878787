c61af83d0f9d84741e1bd3607ec63977f4495f37
Nawaf RIhani <nrihani@myseneca.ca>
2025-01-23 19:34:58 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Author: Nawaf Rihani
+#Author ID: nrihani
+#Date Created: 2025/01/23
+
+import sys
+
+
+
+timer = int(sys.argv[1])
+
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')

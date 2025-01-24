7ec67db7c642cd30739dc25e04931b136a83af89
Nawaf RIhani <nrihani@myseneca.ca>
2025-01-23 19:34:48 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author: Nawaf Rihani
+#Author ID: nrihani
+#Date Created: 2025/01/23
+
+timer = 10
+
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
